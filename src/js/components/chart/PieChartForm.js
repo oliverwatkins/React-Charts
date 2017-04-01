f3baf978b3c912ba1f8e11a98ec09a7d72760d55
watkinso <watkinso@hansaluftbild.de>
2017-04-01 17:19:49 +0200

    pie form

--- src/js/components/chart/PieChartForm.js
+++ src/js/components/chart/PieChartForm.js
@@ -21,12 +21,37 @@ export default class PieChartForm extends React.Component {
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
+                <div>
+                    <h3>Enter Chart Title Here : </h3>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="css-form">
+                    <div className="col-xs-10">
+                        <div className="col-xs-9">
+                            <h3>Add Sections Here :</h3>
+                        </div>
+                    </div>
+                    <div className="input-group col-xs-9">
+                        <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
+                        <input type="text" className="form-control"
+                               name="newSlice" placeholder="Add Pie Slice Name"></input>
+                    </div>
+                    <div className="input-group col-xs-9">
+                        <span className="input-group-addon"><i className="glyphicon glyphicon-plus"></i></span>
+                        <input type="number" className="form-control"
+                               name="newValue" min="1" max="5" placeholder="Add value"></input>
+                    </div>
+
+                    <div className="input-group col-xs-5">
+                        <span className="input-group-addon"><i className="glyphicon glyphicon-tint"></i></span>
+                        <input type="text" className="form-control" name="color" placeholder="Choose Color"></input>
+                    </div>
+
+                    <button type="submit" value="Add Slice" className="button">Add Slice</button>
+
+                </div>
             </form>
+
         );
     }
 }
