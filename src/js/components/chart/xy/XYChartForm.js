d54ec3402cd5b9f0ad18a6f4c493c8e3379c50d5
watkinso <watkinso@hansaluftbild.de>
2017-08-25 12:40:25 +0200

    adazzle grid working

--- src/js/components/chart/xy/XYChartForm.js
+++ src/js/components/chart/xy/XYChartForm.js
@@ -1,14 +1,126 @@
 import React from "react";
+import TitleComponent from '../TitleComponent'
 
+import {CirclePicker} from 'react-color';
+
+import {Modal, Button} from 'react-bootstrap';
 
 export default class XYChartForm extends React.Component {
   constructor(props) {
     super(props)
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.cancel = this.cancel.bind(this);
+    this.popupPicker = this.popupPicker.bind(this);
+    this.chooseColor = this.chooseColor.bind(this);
+
+    this.state = {
+      showModalColorPicker: false
+    };
   }
 
   render() {
+
+    var style = {
+      color: 'black',
+      background: this.state.color
+    };
+
+
     return (
-      <div>TODO</div>
+      <form onSubmit={this.handleSubmit}>
+        <TitleComponent onChange={this.handleChartNameChange}/>
+
+
+
+
+
+
+
+
+        <div className="css-form">
+          <div className="col-xs-10">
+            <div className="col-xs-9">
+              <h3>Add Series Here :</h3>
+            </div>
+          </div>
+          <div className="input-group col-xs-9">
+                        <span className="input-group-addon">
+                            <i className="glyphicon glyphicon-user"></i></span>
+            <input type="text" ref="sliceName" className="form-control" onChange={this.handleChartSliceChange}
+                   name="newSlice" placeholder="Series Name"></input>
+          </div>
+          <div className="input-group col-xs-5">
+            <span className="input-group-addon"><i className="glyphicon glyphicon-tint"></i></span>
+
+            <Modal show={this.state.showModalColorPicker} onHide={this.cancel}>
+              <Modal.Header closeButton>
+                <Modal.Title>Pick a Color</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <CirclePicker onChangeComplete={ this.chooseColor }/>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={this.chooseColor}>OK</Button>
+                <Button onClick={this.cancel}>Cancel</Button>
+              </Modal.Footer>
+            </Modal>
+
+            <input type="text" ref="color" className="form-control"
+                   name="color" placeholder="Choose Color" value={this.state.color} style={style}/>
+            <Button onClick={this.popupPicker}>Choose</Button>
+          </div>
+
+          <div className="input-group col-xs-9">
+            <span className="input-group-addon"><i className="glyphicon glyphicon-plus"></i></span>
+            <input type="number" ref="value" className="form-control"
+                   name="newValue" placeholder="Add values"></input>
+          </div>
+
+          <button type="submit" value="Add Slice" className="button">Add Slice</button>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </form>
     )
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // const val = ReactDOM.findDOMNode(this.refs.sliceName).value;
+    // const color = ReactDOM.findDOMNode(this.refs.color).value;
+    // const value = parseInt(ReactDOM.findDOMNode(this.refs.value).value);
+    //
+    // var slice = {
+    //   name: val,
+    //   color: color,
+    //   value: value
+    // }
+    // PieChartActions.createSlice(slice);
+  }
+
+  cancel(event) {
+    this.setState({showModalColorPicker: false});
+  }
+
+  chooseColor(event) {
+    this.setState({showModalColorPicker: false, color: event.hex});
+  }
+
+  popupPicker(event) {
+    this.setState({showModalColorPicker: true});
+  }
+
 }
--- src/js/components/chart/xy/XYDataList.js
+++ src/js/components/chart/xy/XYDataList.js
@@ -1,13 +1,55 @@
 import React from "react";
+import ReactDataGrid from "react-data-grid";
 
 export default class XYDataList extends React.Component {
+
+
   constructor(props) {
     super(props)
+    this.createRows();
+    this._columns = [
+      { key: 'id', name: 'ID' },
+      { key: 'title', name: 'Title' },
+      { key: 'count', name: 'Count' } ];
+
+    this.rowGetter = this.rowGetter.bind(this);
+  }
+
+
+  createRows() {
+    let rows = [];
+    for (let i = 1; i < 1000; i++) {
+      rows.push({
+        id: i,
+        title: 'Title ' + i,
+        count: i * 1000
+      });
+    }
+
+    this._rows = rows;
+  }
+
+  rowGetter(i) {
+    return this._rows[i];
   }
 
   render() {
+    this._columns = [
+      { key: 'id', name: 'ID' },
+      { key: 'title', name: 'Title' },
+      { key: 'count', name: 'Count' } ];
     return (
-      <div>TODO2</div>
+      <div>
+        <ReactDataGrid
+          columns={this._columns}
+          rowGetter={this.rowGetter}
+          rowsCount={this._rows.length}
+          minHeight={500} />
+
+        TODO2
+
+      </div>
+
     )
   }
 }
