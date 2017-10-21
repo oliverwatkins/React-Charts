df632299c9fb00344eeab9d0ed41a69805c80ba1
watkinso <watkinso@hansaluftbild.de>
2017-10-21 15:25:32 +0200

    package json very broken, but enzyme tests still work

--- src/js/client.js
+++ src/js/client.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-
-
-
-
 import PageLayout from "./PageLayout";
 import BarChartPage from "./pages/BarChartPage";
 import PieChartPage from "./pages/PieChartPage";
