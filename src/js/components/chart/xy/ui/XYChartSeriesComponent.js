bbe5465c46ac83756d9594f45d45ee01dfceda04
watkinso <watkinso@hansaluftbild.de>
2018-06-18 20:26:38 +0200

    react tabs and create series

--- src/js/components/chart/xy/ui/XYChartSeriesComponent.js
+++ src/js/components/chart/xy/ui/XYChartSeriesComponent.js
@@ -6,6 +6,7 @@ import XYChartForm from "./LineChartForm";
 
 import './List.less';
 
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 /**
  * Manages creating deleting series for chart.
  *
@@ -16,15 +17,9 @@ export default class XYChartSeriesComponent extends React.Component {
     super(props);
   }
 
-  // keyPress(e) {
-  //   e.persist();
-  //   let val = this.refs.titleField.input.value
-  //   this.props.onChange(val);
-  // }
-
   handleCreateSeries() {
 
-    alert("create")
+    this.props.createXYSeries({name: "xxx", color: "red"})
   }
 
   onChange (seriesName, axis, row, e) {
@@ -48,38 +43,43 @@ export default class XYChartSeriesComponent extends React.Component {
 
     let series = this.props.xySeries;
 
-    let seriesC = <div>{
-      series.map((val) => <div>
-          <div>
-            {val.name} - {val.color}
-          </div>
-          <form>
-          {
-            val.data.map((v, row) => <div>
-              <span>
-                <input onChange={(e) => this.onChange(val.name, "x", row, e)} ref={"input_x_" + row + "_" + val.name} type="text" value={v.x}/>
-                <input onChange={(e) => this.onChange(val.name, "y", row, e)} ref={"input_y_" + row + "_" + val.name} type="text" value={v.y}/>
-              </span>
-            </div>)
-          }
+    let seriesC =
 
-          <span><input value={"empy1"}/><input value={"empy2"}/></span>
-
-          </form>
-        </div>
-      )
-    }
-    </div>
-
-    return (
-
-
-      <div className="listStyle" style={style}>
-
-        <input value="Create Series" type="button" onClick={this.handleCreateSeries}/>
-        {
-          seriesC
+      <Tabs>
+        <TabList>{
+          series.map((val) =>
+            <Tab>
+              <div>
+                {val.name} - {val.color}
+              </div>
+            </Tab>
+          )
         }
+        </TabList>
+        {
+        series.map((val) =>
+          <TabPanel>
+            <form>
+              {
+                val.data.map((v, row) => <div>
+                  <span>
+                    <input onChange={(e) => this.onChange(val.name, "x", row, e)} ref={"input_x_" + row + "_" + val.name} type="text" value={v.x}/>
+                    <input onChange={(e) => this.onChange(val.name, "y", row, e)} ref={"input_y_" + row + "_" + val.name} type="text" value={v.y}/>
+                  </span>
+                </div>)
+              }
+              <span><input value={"empty1"}/><input value={"empty2"}/></span>
+            </form>
+          </TabPanel>
+          )
+        }
+      </Tabs>
+        return (
+        <div className="listStyle" style={style}>
+          <input value="Create Series" type="button" onClick={(e) => this.handleCreateSeries()}/>
+          {
+            seriesC
+          }
       </div>
     );
   }
